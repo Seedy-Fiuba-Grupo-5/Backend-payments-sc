d8c150851b825f89217e73e272384e9d34ea4360
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-06-20 20:18:48 -0300

    add: Adding Json RPC to connect to local hardhat node

--- src/config.js
+++ src/config.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
-const network = "kovan";
+const ethers = require("ethers");
+
+const node_env = process.env.NODE_ENV;
+temp_network = '';
+if (node_env == 'development') {
+  temp_network = 'localhost';
+} else {
+  temp_network = 'kovan'
+}
+const network = temp_network;
 const deployArtifact = require(`../deployments/${network}/Seedifyuba`);
 const deployerMnemonic = process.env.MNEMONIC;
 const infuraApiKey = process.env.INFURA_API_KEY;
-
+const hh_node_local_url = 'http://localhost:8545'
 console.log(deployerMnemonic);
 module.exports = {
   contractAddress: deployArtifact.address,
@@ -11,4 +20,7 @@ module.exports = {
   deployerMnemonic,
   infuraApiKey,
   network,
+  node_env,
+  hh_node_local_url
 };
+
--- src/services/wallets.js
+++ src/services/wallets.js
@@ -1,13 +1,22 @@
 const ethers = require("ethers");
 const accounts = [];
 
+function create_ethers_provider(config) {
+  if (config.node_env == 'development') {
+    return new ethers.providers.JsonRpcProvider(config.hh_node_local_url);
+  }
+  return new ethers.providers.InfuraProvider(config.network, config.infuraApiKey);
+}
+
 const getDeployerWallet = ({ config }) => () => {
-  const provider = new ethers.providers.InfuraProvider(config.network, config.infuraApiKey);
+  //const provider = new ethers.providers.InfuraProvider(config.network, config.infuraApiKey);
+  const provider = create_ethers_provider(config);
   return ethers.Wallet.fromMnemonic(config.deployerMnemonic).connect(provider);
 };
 
-const createWallet = () => async () => {
-  const provider = new ethers.providers.InfuraProvider("kovan", process.env.INFURA_API_KEY);
+const createWallet = ({ config }) => async () => {
+  // const provider = new ethers.providers.InfuraProvider("kovan", process.env.INFURA_API_KEY);
+  const provider = create_ethers_provider(config);
   // This may break in some environments, keep an eye on it
   const wallet = ethers.Wallet.createRandom().connect(provider);
   accounts.push({
@@ -30,9 +39,9 @@ const getWalletData = () => index => {
   return accounts[index - 1];
 };
 
-const getWallet = ({}) => index => {
-  const provider = new ethers.providers.InfuraProvider("kovan", process.env.INFURA_API_KEY);
-
+const getWallet = ( {config} ) => index => {
+  // const provider = new ethers.providers.InfuraProvider("kovan", process.env.INFURA_API_KEY);
+  const provider = create_ethers_provider(config);
   return new ethers.Wallet(accounts[index - 1].privateKey, provider);
 };
 
