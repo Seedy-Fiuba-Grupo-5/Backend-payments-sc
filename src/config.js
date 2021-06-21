1a2c3108e8c83f4f56193f3359c645ef0de16686
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-06-21 13:20:17 -0300

    add: docker-compose up hh_node and web service

--- src/config.js
+++ src/config.js
@@ -12,7 +12,9 @@ const network = temp_network;
 const deployArtifact = require(`../deployments/${network}/Seedifyuba`);
 const deployerMnemonic = process.env.MNEMONIC;
 const infuraApiKey = process.env.INFURA_API_KEY;
-const hh_node_local_url = 'http://localhost:8545'
+const hh_node_local_url = 'http://172.31.0.2:8545'
+// Should use ipv4_address set in docker-compose.yml
+
 console.log(deployerMnemonic);
 module.exports = {
   contractAddress: deployArtifact.address,
