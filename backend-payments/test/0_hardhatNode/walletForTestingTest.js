b071c5c81ee62f758166c510b1b430feac03a568
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-06-30 14:38:07 -0300

    refactor: rename to walletForTestingTest.js

--- backend-payments/test/0_hardhatNode/walletForTestingTest.js
+++ backend-payments/test/0_hardhatNode/walletForTestingTest.js
@@ -2,14 +2,14 @@
 
 const expect = require('chai').expect;
 const ethers = require("ethers");
-const config = require('../../src/config')
+const config = require('../../src/config');
 
 const provider = new ethers.providers.JsonRpcProvider(config.hh_node_url);
 const testWallet = ethers.Wallet.fromMnemonic(config.deployerMnemonic).connect(provider);
 
 describe("Hardhat node's wallet for testing: ", async () => {
 
-	it('should have 9999.990124216 ethers', async () => {
+	it('should have 9999.990124216 ethers at the beginning', async () => {
     const address = await testWallet.getAddress();
     weisBalance = await provider.getBalance(address);
     ethersBalance = ethers.utils.formatEther(weisBalance);
