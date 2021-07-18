9a386920091aef4406c77d24d51e0a2ac974976d
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-07-18 16:43:15 -0300

    rm: walletsRepo old methods

--- backend-payments/src/db/repositories/walletsRepo.js
+++ backend-payments/src/db/repositories/walletsRepo.js
@@ -6,21 +6,6 @@ async function create(dataDict) {
   return walletRepr;
 }
 
-async function getWalletDB(publicId) {
-  const t = await db.transaction();
-  try {
-    walletRepr = await WalletDB.findByPk(
-      publicId,
-      { transaction: t }
-    );
-    t.commit();
-    return walletRepr;
-  } catch (error) {
-    t.rollback();
-    throw error;
-  }
-}
-
 async function get(publicId) {
   const t = await db.transaction();
   try {
@@ -50,23 +35,7 @@ async function getAll() {
   }
 }
 
-async function getAllWalletsDB() {
-  const t = await db.transaction();
-  try {
-    allWallets = await WalletDB.findAll(
-      { transaction: t }
-    );
-    t.commit();
-    return allWallets;
-  } catch (error) {
-    t.rollback();
-    throw error;
-  }
-}
-
 module.exports = {
-  getAllWalletsDB,
-  getWalletDB,
   get,
   getAll,
   create
