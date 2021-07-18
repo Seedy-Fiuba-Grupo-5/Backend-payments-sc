d1814fc06e7e34a290b3941e531161808a3e9d20
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-07-18 16:36:18 -0300

    rm: projectsRepo old methods

--- backend-payments/src/db/repositories/projectsRepo.js
+++ backend-payments/src/db/repositories/projectsRepo.js
@@ -1,27 +1,8 @@
 const { ProjectDB } = require("../models/project");
 const { db } = require("../db");
 
-async function createProjectDB(dataDict) {
-  await ProjectDB.create(dataDict);
-}
-
 async function create(dataDict) {
   await ProjectDB.create(dataDict);
-}
-
-async function getProjectDB(publicId) {
-  const t = await db.transaction();
-  try {
-    projectRepr = await ProjectDB.findByPk(
-      publicId,
-      { transaction: t }
-    );
-    t.commit();
-    return projectRepr;
-  } catch (error) {
-    t.rollback();
-    throw error;
-  }
 }
 
 async function get(publicId) {
@@ -35,22 +16,6 @@ async function get(publicId) {
     return projectRepr;
   } catch (error) {
     t.rollback();
-    throw error;
-  }
-}
-
-async function updateProjectDB(publicId, updatesDict) {
-  const t = await db.transaction();
-  try {
-    await ProjectDB.update(
-      updatesDict,
-      {
-        where: { publicId: publicId },
-        transaction: t
-      });
-    await t.commit();
-  } catch (error) {
-    await t.rollback();
     throw error;
   }
 }
@@ -72,9 +37,6 @@ async function update(publicId, updatesDict) {
 }
 
 module.exports = {
-  createProjectDB,
-  getProjectDB,
-  updateProjectDB,
   get,
   create,
   update
