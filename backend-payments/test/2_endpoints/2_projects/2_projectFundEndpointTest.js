15420e19c9060f0ef8d5e89c2c323634d26e42ae
Hugo <hlarrea@fi.uba.ar>
2021-07-22 15:32:17 -0300

    Se agregan mas tests :3
    Se agrega test de un usuario foundeando lo justo y necesario para el proyecto y foundeando mas de lo necesario

--- backend-payments/test/2_endpoints/2_projects/2_projectFundEndpointTest.js
+++ backend-payments/test/2_endpoints/2_projects/2_projectFundEndpointTest.js
@@ -124,5 +124,71 @@ describe('Endpoint /projects/<id>/funds: ',()=>{
       expect(res.body).to.have.property('transactionType').to.be.eql('fund');
       expect(res.body).to.have.property('transactionState').to.be.eql('mining');
     });
+
+    it( 'POST extactly total weis needed for a project should add that amount of weis to the project balance, ' +
+        'if the funder has enough ethers to make this transaction', async function () {
+      let [funderRes] = await postManyNewWallets(chai, 1);
+      fundWeis = 6;
+      costTxWeis = 910992000000006;
+      totalWeis = fundWeis + costTxWeis;
+      await addWeis(funderRes.body['address'], totalWeis);
+
+      payload = {
+        "userPublicId": funderRes.body['publicId'],
+        "amountEthers": weisToEthers(fundWeis)
+      };
+
+      res = await chai.request(url)
+                      .post(route)
+                      .set(headersPayload)
+                      .send(payload)
+                      .catch(function(err) {
+                        console.log('DEBUG ERROR');
+                        throw err;
+                      });
+
+      expect(res.status).to.be.eql(202);
+      expect(res.body).to.have.property('amountEthers').to.be.eql(weisToEthers(fundWeis).toString());
+      expect(res.body).to.have.property('fromPublicId').to.be.eql(funderRes.body['publicId']);
+      expect(res.body).to.have.property('fromType').to.be.eql('user');
+      expect(res.body).to.have.property('toPublicId').to.be.eql(fundingProjectRes.body['publicId']);
+      expect(res.body).to.have.property('toType').to.be.eql('project');
+      expect(res.body).to.have.property('transactionType').to.be.eql('fund');
+      expect(res.body).to.have.property('transactionState').to.be.eql('mining');
+    });
+
+    it( 'POST more than total weis needed for a project should add just the amount of weis needed for the project balance, ' +
+        'if the funder has enough ethers to make this transaction', async function () {
+      let [funderRes] = await postManyNewWallets(chai, 1);
+      fundWeis = 7;
+      fundNeeded = 6;
+      costTxWeis = 972976000000007;
+      totalWeis = fundWeis + costTxWeis;
+      await addWeis(funderRes.body['address'], totalWeis);
+
+      payload = {
+        "userPublicId": funderRes.body['publicId'],
+        "amountEthers": weisToEthers(fundWeis)
+      };
+
+      res = await chai.request(url)
+                      .post(route)
+                      .set(headersPayload)
+                      .send(payload)
+                      .catch(function(err) {
+                        console.log('DEBUG ERROR');
+                        throw err;
+                      });
+
+      expect(res.status).to.be.eql(202);
+      expect(res.body).to.have.property('amountEthers').to.be.eql(weisToEthers(fundWeis).toString());
+      expect(res.body).to.have.property('fromPublicId').to.be.eql(funderRes.body['publicId']);
+      expect(res.body).to.have.property('fromType').to.be.eql('user');
+      expect(res.body).to.have.property('toPublicId').to.be.eql(fundingProjectRes.body['publicId']);
+      expect(res.body).to.have.property('toType').to.be.eql('project');
+      expect(res.body).to.have.property('transactionType').to.be.eql('fund');
+      expect(res.body).to.have.property('transactionState').to.be.eql('mining');
+      // TERMINAR - VALIDAR QUE UNA VEZ TERMINADA LA TRANSACCION SOLO SE FOUNDEO LO NECEARIO
+    });
   });
 });
